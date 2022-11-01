729fec34adb92a3223e0ee155717311b62ed0fec
yuan <nguyenshort@gmail.com>
2022-11-01 15:11:39 +0700

    init project

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,40 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-    modules: ['@nuxtjs/tailwindcss', '@nuxtjs/apollo', '@vueuse/nuxt'],
+    modules: ['@nuxtjs/tailwindcss', '@nuxtjs/apollo', '@vueuse/nuxt', '@intlify/nuxt3'],
     apollo: {
         clients: {
             default: './apollo/shinzo/index.ts'
         },
     },
+    intlify: {
+        localeDir: 'lang',
+        vueI18n: {
+            locale: 'ja',
+            messages: {
+                en: {
+                    hello: 'Hello, {name}!',
+                    language: 'Language',
+                    menu: {
+                        home: 'Home',
+                        about: 'About'
+                    },
+                    about: {
+                        description: 'This is about page'
+                    }
+                },
+                ja: {
+                    hello: 'こんにちは、{name}！',
+                    language: '言語',
+                    menu: {
+                        home: 'ホーム',
+                        about: 'アバウト'
+                    },
+                    about: {
+                        description:
+                            'このページのこのサイトの概要について書かれたページです。'
+                    }
+                }
+            }
+        }
+    }
 })
