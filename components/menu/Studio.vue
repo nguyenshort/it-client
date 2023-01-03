d44e9539284c6a17b5f54641c35b8ef5010340e4
yuan <nguyenshort@gmail.com>
2023-01-03 19:59:55 +0700

    cm

--- components/menu/Studio.vue
+++ components/menu/Studio.vue
@@ -1,6 +1,7 @@
 <template>
   <button
     class="ml-6 text-[12px] w-full bg-gradient-to-r from-primary-500 to-primary-600 text-white rounded-full shadow-lg shadow-primary-200 py-2 px-4 flex items-center"
+    @click="openStudio"
   >
     <Icon name="ic:sharp-plus" class="text-[18px] mr-0.5" />
     <span class="font-semibold text-[14px]">
